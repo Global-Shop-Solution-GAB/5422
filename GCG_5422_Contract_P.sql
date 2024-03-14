8b4befc61f81d0d3c8e658bd68fa338917a6f71b
Service Web <sw@gssmail.com>
2024-03-14 10:08:51 -0500

    Commit changes in C:\Websites\Git\emarlow\5422\GCG_5422_Contract_P.sql

---
+++ GCG_5422_Contract_P.sql
@@ -0,0 +1,15 @@
+/* ==========================================
+ * TABLE: GCG_5422_Contract_P
+ * ========================================== */
+CREATE TABLE "GCG_5422_Contract_P"(
+ "Part" CHAR(20) NOT NULL ,
+ "Description" CHAR(30) NOT NULL ,
+ "PL" CHAR(2),
+ "Customer" CHAR(6),
+ "CustomerName" CHAR(30),
+ "Qty" INTEGER,
+ "Price" DOUBLE,
+ "ContractPrice" DOUBLE,
+ "Percentage" DOUBLE);
+
+
